9666bc2315665a797efb35acdcd4509f7aaed547
AzizbekAkhmadjanov <164237536+AzizbekAkhmadjanov@users.noreply.github.com>
2025-09-21 19:51:02 +0500

    Update task 1.sql

--- lesson-5/homework/task 1.sql
+++ lesson-5/homework/task 1.sql
@@ -1,18 +1,9 @@
---Lesson 5: Aliases, Unions, and Conditional columns
---All the Tasks should be solved using/in MSSQL-SERVER.
 
---✅ Aliases ✅ UNION, UNION ALL, INTERSECT, EXCEPT ✅ Creating Conditional Columns with CASE and IIF ✅ Using IF, WHILE
+--CREATE DATABASE HOMETASK5
+--GO
+--USE HOMETASK5;
 
---Notes before doing the tasks:
-
---Tasks should be solved using SQL Server.
---Case insensitivity applies.
---Alias names do not affect the score.
---Scoring is based on the correct output.
---One correct solution is sufficient.
-
---CREATE DATABASE HOMETASK;
---USE HOMETASK;
+-------------------------------------------------------------------------------
 
 --CREATE TABLE Products (
 --    ProductID INT PRIMARY KEY,
@@ -429,136 +420,174 @@
 --(38, 'Ronald', NULL, 'IT', 68000.00, '2017-02-04', 39, NULL, 'Germany'),
 --(39, 'Angela', 'Jenkins', 'Finance', 52000.00, '2018-04-23', 34, 'angelaj@example.com', 'Canada'),
 --(40, 'Gary', 'Wright', 'Marketing', 87000.00, '2021-01-10', 29, NULL, 'UK');
-----------------------------------------------------------------------------------------------------------------
 
-----------------------------------------------------------------------------------------------------------------
+-------------------------------------------------------------------------------
 --Easy-Level Tasks
 --1. Write a query that uses an alias to rename the ProductName column as Name in the Products table.
---SELECT ProductName AS Name
+----SELECT * FROM Products;
+
+--SELECT ProductID,
+--		ProductName AS Name,
+--		Price,
+--		Category,
+--		StockQuantity
 --FROM Products;
 
-----------------------------------------------------------------------------------------------------------------
+-------------------------------------------------------------------------------
 --2. Write a query that uses an alias to rename the Customers table as Client for easier reference.
---SELECT Client.CustomerID,
---       Client.FirstName,
---       Client.LastName,
---       Client.Country
---FROM Customers AS Client;
+----SELECT * FROM Customers;
 
-----------------------------------------------------------------------------------------------------------------
---3. Use UNION to combine results from two queries that select ProductName from Products and ProductName from Products_Discounted.
--- removes duplicates
---SELECT ProductName
---FROM Products
+--SELECT * FROM Customers AS Client;
+
+-------------------------------------------------------------------------------
+--3. Use UNION to combine results from two queries that select ProductName from 
+--	Products and ProductName from Products_Discounted.
+--SELECT ProductName FROM Products;
+--SELECT ProductName FROM Products_Discounted;
+
+-------------------------------
+
+--SELECT ProductName FROM Products
 --UNION
---SELECT ProductName
---FROM Products_Discounted;
+--SELECT ProductName FROM Products_Discounted;
 
-----------------------------------------------------------------------------------------------------------------
+-------------------------------------------------------------------------------
 --4. Write a query to find the intersection of Products and Products_Discounted tables using INTERSECT.
---SELECT ProductName
---FROM Products
---INTERSECT
---SELECT ProductName
---FROM Products_Discounted;
+----SELECT * FROM Products;
+----SELECT * FROM Products_Discounted;
 
-----------------------------------------------------------------------------------------------------------------
+-------------------------------
+--SELECT * FROM Products
+--INTERSECT
+--SELECT * FROM Products_Discounted;
+
+-------------------------------
+--SELECT ProductName FROM Products
+--INTERSECT
+--SELECT ProductName FROM Products_Discounted;
+
+-------------------------------------------------------------------------------
 --5. Write a query to select distinct customer names and their corresponding Country using SELECT DISTINCT.
---SELECT DISTINCT 
---       COALESCE(FirstName, '') + ' ' + COALESCE(LastName, '') AS FullName,
---       Country
+--SELECT * FROM Customers;
+
+--SELECT DISTINCT FirstName, LastName, Country 
 --FROM Customers;
 
+-------------------------------------------------------------------------------
+--6. Write a query that uses CASE to create a conditional column that displays 
+--	'High' if Price > 1000, and 'Low' if Price <= 1000 from Products table.
+----SELECT * FROM Products;
 
-----------------------------------------------------------------------------------------------------------------
---6. Write a query that uses CASE to create a conditional column that displays 'High' if Price > 1000, and 'Low' if Price <= 1000 from Products table.
---SELECT ProductID, ProductName, Price,
---       CASE WHEN Price > 1000 THEN 'High' ELSE 'Low' END AS PriceLevel
+--SELECT *, CASE WHEN Price > 1000 THEN 'High'
+--			ELSE 'Low'
+--			END AS PriceCategory 
 --FROM Products;
 
-----------------------------------------------------------------------------------------------------------------
---7. Use IIF to create a column that shows 'Yes' if StockQuantity > 100, and 'No' otherwise (Products_Discounted table, StockQuantity column).
---SELECT ProductID, ProductName, StockQuantity,
---       IIF(StockQuantity > 100, 'Yes', 'No') AS StockAbove100
+-------------------------------------------------------------------------------
+--7. Use IIF to create a column that shows 'Yes' if StockQuantity > 100, and 
+--	'No' otherwise (Products_Discounted table, StockQuantity column).
+--SELECT * FROM Products_Discounted;
+
+--SELECT
+--	ProductID,
+--	ProductName,
+--	Price,
+--	Category,
+--	StockQuantity,
+--	IIF(StockQuantity> 100, 'Yes', 'No') AS	InStock
 --FROM Products_Discounted;
 
-----------------------------------------------------------------------------------------------------------------
+-------------------------------------------------------------------------------
 --Medium-Level Tasks
---8. Use UNION to combine results from two queries that select ProductName from Products and ProductName from Products_Discounted tables.
--- distinct results (UNION)
+--8. Use UNION to combine results from two queries that select ProductName from 
+--	Products and ProductName from Products_Discounted tables.
+
 --SELECT ProductName FROM Products
 --UNION
 --SELECT ProductName FROM Products_Discounted;
 
----- include duplicates (UNION ALL)
+-------------------------------------------------------------------------------
+--9. Write a query that returns the difference between the Products and 
+--	Products_Discounted tables using EXCEPT.
+
 --SELECT ProductName FROM Products
---UNION ALL
---SELECT ProductName FROM Products_Discounted;
-
-----------------------------------------------------------------------------------------------------------------
---9. Write a query that returns the difference between the Products and Products_Discounted tables using EXCEPT.
---SELECT ProductName
---FROM Products
 --EXCEPT
---SELECT ProductName
---FROM Products_Discounted;
+--SELECT ProductName FROM Products_Discounted
 
-----------------------------------------------------------------------------------------------------------------
---10. Create a conditional column using IIF that shows 'Expensive' if the Price is greater than 1000, and 'Affordable' if less, from Products table.
---SELECT ProductID, ProductName, Price,
---       IIF(Price > 1000, 'Expensive', 'Affordable') AS PriceCategory
+-------------------------------------------------------------------------------
+--10. Create a conditional column using IIF that shows 'Expensive' if the Price
+--	is greater than 1000, and 'Affordable' if less, from Products table.
+----SELECT * FROM Products;
+
+--SELECT ProductID,
+--		ProductName,
+--		Price, 
+--		Category,
+--		StockQuantity,
+--		IIF(Price>1000, 'Expensive', 'Affordable') AS PriceCategory
 --FROM Products;
 
-----------------------------------------------------------------------------------------------------------------
---11. Write a query to find employees in the Employees table who have either Age < 25 or Salary > 60000.
---SELECT *
---FROM Employees
---WHERE Age < 25
---   OR Salary > 60000;
+-------------------------------------------------------------------------------
+--11. Write a query to find employees in the Employees table 
+--	who have either Age < 25 or Salary > 60000.
+----SELECT * FROM Employees;
 
-----------------------------------------------------------------------------------------------------------------
---12. Update the salary of an employee based on their department, increase by 10% if they work in 'HR' or EmployeeID = 5
--- show old and new using OUTPUT
+--SELECT * FROM Employees
+--WHERE Age<25 OR Salary > 60000;
+
+-------------------------------------------------------------------------------
+--12. Update the salary of an employee based on their department, increase
+--	by 10% if they work in 'HR' or EmployeeID = 5
+--SELECT * FROM Employees;
+
 --UPDATE Employees
---SET Salary = Salary * 1.10
---OUTPUT inserted.EmployeeID,
---       deleted.Salary AS OldSalary,
---       inserted.Salary AS NewSalary
---WHERE DepartmentName = 'HR'
---   OR EmployeeID = 5;
+--SET Salary= 1.1* Salary
+--WHERE EmployeeID=5 OR DepartmentName= 'HR'
+--SELECT * FROM Employees;
 
-----------------------------------------------------------------------------------------------------------------
+-------------------------------------------------------------------------------
 --Hard-Level Tasks
---13. Write a query that uses CASE to assign 'Top Tier' if SaleAmount > 500, 'Mid Tier' if SaleAmount BETWEEN 200 AND 500, and 'Low Tier' otherwise. (From Sales table)
---SELECT SaleID, ProductID, CustomerID, SaleAmount,
---       CASE
---         WHEN SaleAmount > 500 THEN 'Top Tier'
---         WHEN SaleAmount BETWEEN 200 AND 500 THEN 'Mid Tier'
---         ELSE 'Low Tier'
---       END AS SaleTier
---FROM Sales;
+--13. Write a query that uses CASE to assign 'Top Tier' if SaleAmount > 500, 
+--	'Mid Tier' if SaleAmount BETWEEN 200 AND 500, and 'Low Tier' otherwise. 
+--	(From Sales table)
+--SELECT * FROM Sales;
 
-----------------------------------------------------------------------------------------------------------------
---14. Use EXCEPT to find customers' ID who have placed orders but do not have a corresponding record in the Sales table.
---SELECT DISTINCT CustomerID
---FROM Orders
+--SELECT  *,
+--		CASE 
+--			WHEN SaleAmount>500 THEN 'Top Tier'
+--			WHEN SaleAmount BETWEEN 200 AND 500 THEN 'Mid Tier'
+--			ELSE 'Low Tier'
+--		END AS PriceCategory
+--FROM Sales;
+-------------------------------------------------------------------------------
+--14. Use EXCEPT to find customers' ID who have placed orders 
+--	but do not have a corresponding record in the Sales table.
+--SELECT * FROM Orders;
+--SELECT * FROM Sales;
+
+--SELECT CustomerID FROM Orders
 --EXCEPT
---SELECT DISTINCT CustomerID
---FROM Sales;
+--SELECT CustomerID FROM Sales;
 
-----------------------------------------------------------------------------------------------------------------
---15. Write a query that uses a CASE statement to determine the discount percentage based on the quantity purchased. Use orders table. Result set should show customerid, quantity and discount percentage. The discount should be applied as follows: 1 item: 3% Between 1 and 3 items : 5% Otherwise: 7%
---SELECT CustomerID, Quantity,
---       CASE
---         WHEN Quantity = 1 THEN '3%'
---         WHEN Quantity BETWEEN 2 AND 3 THEN '5%'
---         ELSE '7%'
---       END AS DiscountPercent,
---       CASE
---         WHEN Quantity = 1 THEN 0.03
---         WHEN Quantity BETWEEN 2 AND 3 THEN 0.05
---         ELSE 0.07
---       END AS DiscountRate
+-------------------------------------------------------------------------------
+--15. Write a query that uses a CASE statement to determine the discount 
+	--percentage based on the quantity purchased. Use orders table. 
+	--Result set should show customerid, quantity and discount percentage. 
+	--The discount should be applied as follows: 
+	--1 item: 3% 
+	--Between 1 and 3 items : 5% 
+	--Otherwise: 7%
+
+----SELECT * FROM Orders;
+
+--SELECT  CustomerID,
+--		Quantity,
+--		CASE 
+--			WHEN Quantity=1 THEN '3%'
+--			WHEN Quantity BETWEEN 1 AND 3 THEN '5%'
+--			ELSE '7%'
+--		END AS DiscountPercentage
 --FROM Orders;
 
+-------------------------------------------------------------------------------
 
