ccee934924b6a07c23d6a3a709bcfcea39d3f59f
AzizbekAkhmadjanov <164237536+AzizbekAkhmadjanov@users.noreply.github.com>
2025-09-21 14:14:03 +0500

    Update task 1.sql

--- lesson-4/homework/task 1.sql
+++ lesson-4/homework/task 1.sql
@@ -1,8 +1,10 @@
 
---CREATE DATABASE HOMETASK;
---USE HOMETASK;
---DROP TABLE IF EXISTS Employees;
+--CREATE DATABASE HOMETASK4
+--GO
+--USE HOMETASK4;
 
+-------------------------------------------------------------------------
+--DROP TABLE IF EXISTS Employees;
 
 --CREATE TABLE Employees (
 --    EmployeeID INT PRIMARY KEY,
@@ -340,189 +342,247 @@
 --(39, 39, 40, '2023-11-26', 3, 120.00),
 --(40, 40, 1, '2024-03-09', 1, 1200.00);
 
-----------------------------------------------------------------------------------------------------------
+-------------------------------------------------------------------------
 --Easy-Level Tasks (10)
 --1. Write a query to select the top 5 employees from the Employees table.
---SELECT TOP (5) *
+----SELECT * FROM Employees;
+
+--SELECT TOP 5 * 
 --FROM Employees;
 
-----------------------------------------------------------------------------------------------------------
+-------------------------------------------------------------------------
 --2. Use SELECT DISTINCT to select unique Category values from the Products table.
---SELECT DISTINCT Category
+----SELECT * FROM Products;
+
+--SELECT DISTINCT Category 
 --FROM Products;
 
-----------------------------------------------------------------------------------------------------------
+-------------------------------------------------------------------------
 --3. Write a query that filters the Products table to show products with Price > 100.
---SELECT *
---FROM Products
---WHERE Price > 100;
+----SELECT * FROM Products;
 
-----------------------------------------------------------------------------------------------------------
+--SELECT * FROM Products
+--WHERE Price> 100;
+
+-------------------------------------------------------------------------
 --4. Write a query to select all Customers whose FirstName start with 'A' using the LIKE operator.
---SELECT *
---FROM Customers
---WHERE FirstName LIKE 'A%';
+----SELECT * FROM Customers;
 
-----------------------------------------------------------------------------------------------------------
+--SELECT * FROM Customers
+--WHERE FirstName	LIKE 'A%';
+
+-------------------------------------------------------------------------
 --5. Order the results of a Products table by Price in ascending order.
---SELECT *
---FROM Products
---ORDER BY Price ASC;
+----SELECT * FROM Products;
 
-----------------------------------------------------------------------------------------------------------
+--SELECT * FROM Products
+--ORDER BY Price;
+
+-------------------------------------------------------------------------
 --6. Write a query that uses the WHERE clause to filter for employees with Salary >= 60000 and DepartmentName = 'HR'.
---SELECT *
---FROM Employees
---WHERE Salary >= 60000
---  AND DepartmentName = 'HR';
+----SELECT * FROM Employees;
 
-----------------------------------------------------------------------------------------------------------
+--SELECT * FROM Employees
+--WHERE Salary>=60000 and DepartmentName= 'HR';
+
+-------------------------------------------------------------------------
 --7. Use ISNULL to replace NULL values in the Email column with the text "noemail@example.com".From Employees table
---SELECT EmployeeID,
---       FirstName,
---       LastName,
---       ISNULL(Email, 'noemail@example.com') AS Email
+----SELECT * FROM Employees;
+
+--SELECT 
+--    EmployeeID,
+--    FirstName,
+--    LastName,
+--    DepartmentName,
+--    Salary,
+--    HireDate,
+--    Age,
+--    ISNULL(Email, 'noemail@example.com') AS Email,
+--    Country
 --FROM Employees;
 
-
-----------------------------------------------------------------------------------------------------------
+-------------------------------------------------------------------------
 --8. Write a query that shows all products with Price BETWEEN 50 AND 100.
---SELECT *
---FROM Products
+----SELECT * FROM Products;
+
+--SELECT * FROM Products
 --WHERE Price BETWEEN 50 AND 100;
-
-----------------------------------------------------------------------------------------------------------
+ 
+-------------------------------------------------------------------------
 --9. Use SELECT DISTINCT on two columns (Category and ProductName) in the Products table.
---SELECT DISTINCT Category, ProductName
---FROM Products;
+----SELECT * FROM Products;
 
-----------------------------------------------------------------------------------------------------------
---10. After SELECT DISTINCT on two columns (Category and ProductName) Order the results by ProductName in descending order.
---SELECT DISTINCT Category, ProductName
+--SELECT Category, ProductName FROM Products;
+
+-------------------------------------------------------------------------
+--10. After SELECT DISTINCT on two columns (Category and ProductName) 
+--    Order the results by ProductName in descending order.
+
+--SELECT Category, ProductName 
 --FROM Products
 --ORDER BY ProductName DESC;
 
-----------------------------------------------------------------------------------------------------------
---🟠 Medium-Level Tasks (10)
-----------------------------------------------------------------------------------------------------------
+-------------------------------------------------------------------------
+--Medium-Level Tasks (10)
 --11. Write a query to select the top 10 products from the Products table, ordered by Price DESC.
+----SELECT * FROM Products;
+
 --SELECT TOP 10 *
 --FROM Products
 --ORDER BY Price DESC;
 
-----------------------------------------------------------------------------------------------------------
+-------------------------------------------------------------------------
 --12. Use COALESCE to return the first non-NULL value from FirstName or LastName in the Employees table.
---SELECT EmployeeID,
---       COALESCE(FirstName, LastName) AS NameValue
+--SELECT COALESCE(FirstName, LastName) AS DisplayName 
 --FROM Employees;
 
-----------------------------------------------------------------------------------------------------------
+-------------------------------------------------------------------------
 --13. Write a query that selects distinct Category and Price from the Products table.
---SELECT DISTINCT Category, Price
---FROM Products;
+----SELECT * FROM Products;
 
-----------------------------------------------------------------------------------------------------------
---14. Write a query that filters the Employees table to show employees whose Age is either between 30 and 40 or DepartmentName = 'Marketing'.
---SELECT *
---FROM Employees
---WHERE (Age BETWEEN 30 AND 40)
---   OR DepartmentName = 'Marketing';
+--SELECT DISTINCT Category, Price FROM Products;
 
-----------------------------------------------------------------------------------------------------------
---15. Use OFFSET-FETCH to select rows 11 to 20 from the Employees table, ordered by Salary DESC.
---SELECT *
---FROM Employees
---ORDER BY Salary DESC
---OFFSET 10 ROWS
---FETCH NEXT 10 ROWS ONLY;
+-------------------------------------------------------------------------
+--14. Write a query that filters the Employees table to show employees 
+--     whose Age is either between 30 and 40 or DepartmentName = 'Marketing'.
+----SELECT * FROM Employees;
 
-----------------------------------------------------------------------------------------------------------
---16. Write a query to display all products with Price <= 1000 and StockQuantity > 50, sorted by Stock in ascending order.
---SELECT *
---FROM Products
---WHERE Price <= 1000
---  AND StockQuantity > 50
---ORDER BY StockQuantity ASC;
+--SELECT * FROM Employees
+--WHERE (Age BETWEEN 30 AND 40) OR DepartmentName= 'Marketing';
 
-----------------------------------------------------------------------------------------------------------
---17. Write a query that filters the Products table for ProductName values containing the letter 'e' using LIKE.
---SELECT *
---FROM Products
+-------------------------------------------------------------------------
+--15. Use OFFSET-FETCH to select rows 11 to 20 from the Employees table, 
+--		ordered by Salary DESC.
+----SELECT * FROM Employees;
+
+--SELECT * FROM Employees
+--ORDER BY Salary DESC OFFSET 10 ROWS FETCH NEXT 10 ROWS ONLY;
+
+-------------------------------------------------------------------------
+--16. Write a query to display all products with Price <= 1000 and 
+--	StockQuantity > 50, sorted by Stock in ascending order.
+----SELECT * FROM Products;
+
+--SELECT * FROM Products
+--WHERE Price<=1000 AND StockQuantity>50
+--ORDER BY StockQuantity;
+
+-------------------------------------------------------------------------
+--17. Write a query that filters the Products table for 
+--	ProductName values containing the letter 'e' using LIKE.
+----SELECT * FROM Products;
+
+--SELECT * FROM Products
 --WHERE ProductName LIKE '%e%';
 
-----------------------------------------------------------------------------------------------------------
+-------------------------------------------------------------------------
 --18. Use IN operator to filter for employees who work in either 'HR', 'IT', or 'Finance'.
---SELECT *
---FROM Employees
---WHERE DepartmentName IN ('HR', 'IT', 'Finance');
+----SELECT * FROM Employees;
 
-----------------------------------------------------------------------------------------------------------
---19. Use ORDER BY to display a list of customers ordered by City in ascending and PostalCode in descending order.Use customers table
---SELECT *
---FROM Customers
---ORDER BY City ASC, PostalCode DESC;
+--SELECT * FROM Employees
+--WHERE DepartmentName IN ('HR','IT','Finance');
 
-----------------------------------------------------------------------------------------------------------
---🔴 Hard-Level Tasks
---20. Write a query that selects the top 5 products with the highest sales, using TOP(5) and ordered by SaleAmount DESC.
---SELECT TOP (5) *
+-------------------------------------------------------------------------
+--19. Use ORDER BY to display a list of customers ordered by City in 
+--	ascending and PostalCode in descending order.Use customers table
+----SELECT * FROM Customers;
+
+--SELECT * FROM Customers
+--ORDER BY City, PostalCode DESC;
+
+-------------------------------------------------------------------------
+--Hard-Level Tasks
+--20. Write a query that selects the top 5 products with the highest sales,
+--		using TOP(5) and ordered by SaleAmount DESC. 
+----SELECT * FROM Sales;
+
+--SELECT TOP 5 * 
+--FROM Sales
+--ORDER BY SaleAmount DESC;
+
+-------------------------------------------------------------------------
+--21. Combine FirstName and LastName into one column named FullName in the 
+--	Employees table. (only in select statement)
+--SELECT * FROM Employees;
+
+--SELECT (FirstName + ' ' + LastName) AS FullName FROM Employees;
+
+-------------------------------------------------------------------------
+--22. Write a query to select the distinct Category, ProductName, and Price for 
+--	products that are priced above $50, using DISTINCT on three columns.
+----SELECT * FROM Products;
+
+--SELECT DISTINCT Category, ProductName, Price
 --FROM Products
---ORDER BY StockQuantity DESC;
+--WHERE Price > 50
+--ORDER BY Price;
+
+-------------------------------------------------------------------------
+--23. Write a query that selects products whose Price is less than 10% of the 
+--	average price in the Products table. (Do some research on how to find average
+--	price of all products)
+----SELECT * FROM Products;
+
+----SELECT AVG(Price) FROM Products;
+
+--SELECT * FROM Products
+--WHERE Price < 0.1 * (SELECT AVG(Price) FROM Products)
+
+-------------------------------------------------------------------------
+--24. Use WHERE clause to filter for employees whose Age is less than 30
+--	and who work in either the 'HR' or 'IT' department.
+----SELECT * FROM Employees;
+
+--SELECT * FROM Employees
+--WHERE Age<30 AND DepartmentName IN ('HR','IT');
+
+-------------------------------------------------------------------------
+--25. Use LIKE with wildcard to select all customers whose Email contains 
+--	the domain '@gmail.com'.
+----SELECT * FROM Customers;
+
+--SELECT * FROM Customers
+--WHERE Email LIKE '%[@]gmail.com%';
 
 
-----------------------------------------------------------------------------------------------------------
---21. Combine FirstName and LastName into one column named FullName in the Employees table. (only in select statement)
---SELECT 
---    EmployeeID,
---    FirstName + ' ' + LastName AS FullName,
---    Age,
---    DepartmentName,
---    Salary
---FROM Employees;
-
-----------------------------------------------------------------------------------------------------------
---22. Write a query to select the distinct Category, ProductName, and Price for products that are priced above $50, using DISTINCT on three columns.
---SELECT DISTINCT 
---    Category, 
---    ProductName, 
---    Price
---FROM Products
---WHERE Price > 50;
-
-----------------------------------------------------------------------------------------------------------
---23. Write a query that selects products whose Price is less than 10% of the average price in the Products table. (Do some research on how to find average price of all products)
---SELECT AVG(Price) AS AvgPrice
---FROM Products;
---SELECT *
---FROM Products
---WHERE Price < (SELECT AVG(Price) * 0.1 FROM Products);
-
-----------------------------------------------------------------------------------------------------------
---24. Use WHERE clause to filter for employees whose Age is less than 30 and who work in either the 'HR' or 'IT' department.
---SELECT *
---FROM Employees
---WHERE Age < 30
---  AND DepartmentName IN ('HR', 'IT');
-
-----------------------------------------------------------------------------------------------------------
---25. Use LIKE with wildcard to select all customers whose Email contains the domain '@gmail.com'.
---SELECT *
---FROM Customers
+--SELECT * FROM Customers
 --WHERE Email LIKE '%@gmail.com';
 
-----------------------------------------------------------------------------------------------------------
---26. Write a query that uses the ALL operator to find employees whose salary is greater than all employees in the 'Sales' department.
---SELECT *
+-------------------------------------------------------------------------
+--26. Write a query that uses the ALL operator to find employees whose salary
+--	is greater than all employees in the 'Sales' department.
+------SELECT * FROM Employees;
+-----------------------------------
+--SELECT Salary 
 --FROM Employees
---WHERE Salary > ALL (
---    SELECT Salary
---    FROM Employees
---    WHERE DepartmentName = 'Sales'
---);
+--WHERE DepartmentName= 'Sales'
+-----------------------------------
+--SELECT * FROM Employees
+--WHERE Salary> ALL (
+--				SELECT Salary
+--				FROM Employees
+--				WHERE DepartmentName= 'Sales'
+--				);
 
-----------------------------------------------------------------------------------------------------------
---27. Write a query that filters the Orders table for orders placed in the last 180 days using BETWEEN and LATEST_DATE in the table. (Search how to get the current date and latest date)
+
+-------------------------------------------------------------------------
+--27. Write a query that filters the Orders table for orders placed in the 
+--	last 180 days using BETWEEN and LATEST_DATE in the table. 
+--		(Search how to get the current date and latest date)
+--SELECT * FROM Orders;
+
+-----------------------------------
+--SELECT GETDATE();
+
+-----------------------------------
+--SELECT MAX(OrderDate) FROM Orders;
+
+-----------------------------------
+
 --SELECT *
 --FROM Orders
---WHERE OrderDate BETWEEN DATEADD(DAY, -180, GETDATE()) AND GETDATE();
+--WHERE OrderDate BETWEEN DATEADD(DAY, -180, (SELECT MAX(OrderDate) FROM Orders))
+--                    AND (SELECT MAX(OrderDate) FROM Orders);
 
+
+-------------------------------------------------------------------------
